a5e3f5f7c086f9542649a3d3feadf8f63eb862b5
AlexRose2603 <alexandra26korchak@gmail.com>
2023-04-23 13:00:13 +0300

    done

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,44 @@
 import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { RiDeleteBinLine, RiEdit2Line, RiSave3Line } from 'react-icons/ri';
+import { useState } from 'react';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ description, index, id, onDelete, onUpdate }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.currentTarget.value);
+  };
+  const handleClickBtn = () => {
+    setIsEdit(prevState => !prevState);
+    if (isEdit) {
+      onUpdate(value, id);
+    } else {
+      setValue(description);
+    }
+  };
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{index + 1}
+      </Text>
+      {isEdit ? (
+        <input value={value} onChange={handleChange} />
+      ) : (
+        <Text>{description}</Text>
+      )}
+      <DeleteButton
+        type="button"
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+      <EditButton type="button" onClick={handleClickBtn}>
+        {isEdit ? <RiSave3Line size={24} /> : <RiEdit2Line size={24} />}
+      </EditButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,56 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
-export class Todos extends Component {
-  render() {
-    return <Text>Todos</Text>;
-  }
-}
+export const Todos = () => {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(window.localStorage.getItem('todos')) ?? []
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleCreateTodo = value => {
+    const newTodo = { text: value, id: nanoid() };
+    setTodos(prevState => [...prevState, newTodo]);
+  };
+
+  const deleteTodo = removeId => {
+    const updatedTodos = todos.filter(({ id }) => id !== removeId);
+    setTodos(updatedTodos);
+  };
+
+  const updateTodo = (newText, id) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, text: newText };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <SearchForm onSubmit={handleCreateTodo} />
+      <Grid>
+        {todos.length > 0 &&
+          todos.map(({ text, id }, index) => {
+            return (
+              <GridItem key={id}>
+                <Todo
+                  description={text}
+                  index={index}
+                  id={id}
+                  onDelete={deleteTodo}
+                  onUpdate={updateTodo}
+                />
+              </GridItem>
+            );
+          })}
+      </Grid>
+    </>
+  );
+};
